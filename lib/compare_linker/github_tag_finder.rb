eca04d6545dd4fe784f3fc24d3ede0f9d125d460
Takashi Masuda <masutaka.net@gmail.com>
2016-10-09 16:18:58 +0900

    Rescue gems with many tags
    e.g. https://github.com/newrelic/rpm/tags

--- lib/compare_linker/github_tag_finder.rb
+++ lib/compare_linker/github_tag_finder.rb
@@ -7,12 +7,22 @@ class CompareLinker
     end
 
     def find(repo_full_name, gem_version)
-      tags = octokit.tags(repo_full_name)
+      tags = auto_paginate { octokit.tags(repo_full_name) }
       if tags
         tags.find { |tag|
           tag.name == gem_version || tag.name == "v#{gem_version}"
         }
       end
+    end
+
+    private
+
+    def auto_paginate
+      original = octokit.auto_paginate
+      octokit.auto_paginate = true
+      yield
+    ensure
+      octokit.auto_paginate = original
     end
   end
 end
