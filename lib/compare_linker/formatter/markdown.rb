52a46101706d177d68b1ec062ed20b983e46e853
Takashi Masuda <masutaka.net@gmail.com>
2016-10-09 18:42:20 +0900

    Improve output format

--- lib/compare_linker/formatter/markdown.rb
+++ lib/compare_linker/formatter/markdown.rb
@@ -9,17 +9,17 @@ class CompareLinker
         text = "* [ ] "
         text += case
         when g.owner && g.old_rev && g.new_rev
-          "#{g.gem_name}: https://github.com/#{g.owner}/#{g.gem_name}/compare/#{g.old_rev}...#{g.new_rev}"
+          "#{g.gem_name}: [#{g.old_rev}...#{g.new_rev}](https://github.com/#{g.owner}/#{g.gem_name}/compare/#{g.old_rev}...#{g.new_rev})"
         when g.homepage_uri && g.old_tag && g.new_tag
-          "[#{g.gem_name}](#{g.homepage_uri}): https://github.com/#{g.repo_owner}/#{g.repo_name}/compare/#{g.old_tag}...#{g.new_tag}"
+          "[#{g.gem_name}](#{g.homepage_uri}): [#{g.old_ver}...#{g.new_ver}](https://github.com/#{g.repo_owner}/#{g.repo_name}/compare/#{g.old_tag}...#{g.new_tag})"
         when g.homepage_uri
-          "[#{g.gem_name}](#{g.homepage_uri}): #{g.old_ver} => #{g.new_ver}"
+          "[#{g.gem_name}](#{g.homepage_uri}): #{g.old_ver}...#{g.new_ver}"
         when g.old_tag && g.new_tag
-          "#{g.gem_name}: https://github.com/#{g.repo_owner}/#{g.repo_name}/compare/#{g.old_tag}...#{g.new_tag}"
+          "#{g.gem_name}: [#{g.old_ver}...#{g.new_ver}](https://github.com/#{g.repo_owner}/#{g.repo_name}/compare/#{g.old_tag}...#{g.new_tag})"
         when g.repo_owner && g.repo_name
-          "[#{g.gem_name}](https://github.com/#{g.repo_owner}/#{g.repo_name}): #{g.old_ver} => #{g.new_ver}"
+          "[#{g.gem_name}](https://github.com/#{g.repo_owner}/#{g.repo_name}): #{g.old_ver}...#{g.new_ver}"
         else
-          "#{g.gem_name}: (link not found) #{g.old_ver} => #{g.new_ver}"
+          "#{g.gem_name}: (link not found) #{g.old_ver}...#{g.new_ver}"
         end
 
         if downgrade?(g.old_ver, g.new_ver, g.old_tag, g.new_tag)
