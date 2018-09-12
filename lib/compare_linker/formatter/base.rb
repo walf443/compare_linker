1555e7be838822c50ae725baa44f229b9588c1ec
Keiji Yoshimi <walf443@gmail.com>
2018-09-12 19:03:30 +0900

    handle downgrade? error

--- lib/compare_linker/formatter/base.rb
+++ lib/compare_linker/formatter/base.rb
@@ -21,6 +21,8 @@ class CompareLinker
         Gem::Version.new(new_ver) < Gem::Version.new(old_ver) ||
           (old_tag && new_tag && Gem::Version.new(to_ver(new_tag)) < Gem::Version.new(to_ver(old_tag))) ||
           (old_rev && new_rev && Gem::Version.new(to_ver(new_rev)) < Gem::Version.new(to_ver(old_rev)))
+      rescue ArgumentError # Gem::Version raise ArgumentError if it is invalid as version
+        false
       end
 
       def to_ver(tag)
